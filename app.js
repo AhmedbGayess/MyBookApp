ef235fc589c95b431855e8f3fcfec4cf1c9d806f
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2018-05-28 01:11:32 +0100

    Fixed everything
    Rewrote everything from scratch and made it work !

--- app.js
+++ app.js
@@ -1,74 +1,113 @@
-var addBtn = document.querySelector("#add");
-var bookTitle = document.getElementById("book-title");
-var bookCover = document.getElementById("book-cover");
-var bookList = document.getElementById("book-list");
-var emptyList = document.querySelector("#empty-list");
-var deleteBtn = document.querySelector(".delete-btn");
-var myBooks = [];
+const addBtn = document.querySelector("#add");
+const bookTitle = document.getElementById("book-title");
+const bookAuthor = document.getElementById("book-author")
+const bookCover = document.getElementById("book-cover");
+const bookList = document.getElementById("book-list");
+const emptyList = document.querySelector("#empty-list");
 
+// Get books from localStorage
+const getStoredBooks = function () {
+    const storedBooks = localStorage.getItem("Books")
 
-
-addBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    myBooks.push( {title: bookTitle.value, cover: bookCover.value});
-    createBook(bookTitle.value, bookCover.value);
-    bookTitle.value = "";
-    bookCover.value = "";
-    // console.log(myBooks);
-})
-
-document.querySelector("body").addEventListener("click", function(e) {
-    if(e.target.classList.contains("delete-btn")){
-        for (var i =0; i < myBooks.length; i++) {
-            if (myBooks[i].title === e.target.dataset.book) {
-                myBooks.splice(i, 1);
-                clearBooks();
-                renderBook();
-             }
-        }
-   }  
-})
-
-emptyList.addEventListener("click", function(){
-    if (confirm('Are you sure you want to delete all your books from the list?')) {
-        clearBooks();
-    } 
-})
-
-function clearBooks() {
-    while(bookList.firstChild){
-      bookList.removeChild(bookList.firstChild);
+    if (storedBooks !== null) {
+        return JSON.parse(storedBooks)
+    } else {
+        return []
     }
-};
-
-function renderBook() {
-    for(i = 0; i < myBooks.length; i++){
-        createBook(myBooks[i].title, myBooks[i].cover);        
-    };
 }
 
-function createBook(title, cover) {
-    var newBook = document.createElement("li");
-    newBook.classList.add("list-group-item");
-    var newBookTitle = document.createElement("p");
-    newBookTitle.innerHTML = title;
-    newBookTitle.classList.add("li-content")
-    var newBookCover = document.createElement("img");
-    newBookCover.src = cover;
-    newBookCover.classList.add("cover")
-    var deleteBtn = document.createElement("button");
-    deleteBtn.dataset.book = title;
-    deleteBtn.classList.add("btn", "btn-danger", "delete-btn", "li-content");
-    var deleteBtnName = document.createTextNode("Delete Book");
-    deleteBtn.appendChild(deleteBtnName);
-    newBook.appendChild(newBookTitle);
-    newBook.appendChild(newBookCover);
-    newBook.appendChild(deleteBtn);
-    addBooks(newBook);
+let myBooks = getStoredBooks()
+
+
+const renderBooks = function (myBooks) {
+    bookList.innerHTML = ""
+    myBooks.forEach(function (book) {
+        // Book Row
+        const newBookRow = document.createElement("div")
+        newBookRow.classList = "row"
+
+        // Book title
+        const newBookTitle = document.createElement("div")
+        newBookTitle.textContent = book.title
+        newBookTitle.classList = "col list"
+        newBookRow.appendChild(newBookTitle)
+
+        // Book Author
+        const newBookAuthor = document.createElement("div")
+        newBookAuthor.textContent = book.author
+        newBookAuthor.classList = "col list"        
+        newBookRow.appendChild(newBookAuthor)
+
+        // Book Cover
+        const imageCol = document.createElement("div")
+        imageCol.classList = "col"        
+        const newBookCover = document.createElement("img")
+        newBookCover.setAttribute("src", book.cover)
+        newBookCover.classList = "cover"
+        imageCol.appendChild(newBookCover)
+        newBookRow.appendChild(imageCol)
+
+        // Delete button
+        const deleteCol = document.createElement("div")
+        deleteCol.classList = "col list"
+        const deleteBtn = document.createElement("button")
+        deleteBtn.classList = "btn delete"
+        deleteBtn.innerHTML = 'Delete <i class="far fa-trash-alt"></i>'
+        deleteCol.appendChild(deleteBtn)
+        newBookRow.appendChild(deleteCol)
+
+        deleteBtn.addEventListener("click", function () {
+            deleteBook(book.id)
+            saveBooks(myBooks)
+            renderBooks(myBooks)   
+        })
+
+
+        bookList.appendChild(newBookRow)    
+        
+    })
 }
 
-function addBooks(newBook) {
-    bookList.appendChild(newBook);
-};
+renderBooks(myBooks)
 
-renderBook();
+// Delete single book
+const deleteBook = function (id) {
+    const index = myBooks.findIndex(function (book) {
+        return book.id === id
+        })
+        
+        if (index > -1) {
+            myBooks.splice(index, 1)
+    }
+}
+
+// Store the apdated array in localStorage
+const saveBooks = function (myBooks) {
+    localStorage.setItem("Books", JSON.stringify(myBooks))
+}
+
+addBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    // Create book object and push it into the array
+    myBooks.push({
+        id: uuidv4(),
+        title: bookTitle.value,
+        author: bookAuthor.value,
+        cover: bookCover.value
+    })
+
+    saveBooks(myBooks)
+
+    
+    renderBooks(myBooks)
+    bookTitle.value = ""
+    bookAuthor.value = ""
+    bookCover.value = ""
+
+})
+
+emptyList.addEventListener("click", function () {
+    localStorage.clear()
+    myBooks = []
+    renderBooks(myBooks)
+})
